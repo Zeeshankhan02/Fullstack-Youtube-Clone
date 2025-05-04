84321c149fc73df72a44d4a7560d13e3c1afe8c1
Zeeshan Khan <zeeshankhan0271@gmail.com>
2025-05-04 20:35:15 +0530

    updated cors config

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,11 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin:"*",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({limit:'16kb'}))
 app.use(express.static("public"))
