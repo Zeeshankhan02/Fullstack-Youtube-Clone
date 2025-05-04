d12575184b88cebb66058d1a40cd290de5c35b9f
Zeeshan Khan <zeeshankhan0271@gmail.com>
2025-05-04 19:38:56 +0530

    updated health Check Route

--- backend/src/controllers/healthCheck.controller.js
+++ backend/src/controllers/healthCheck.controller.js
@@ -3,7 +3,7 @@ import {asyncHandler} from "../utils/asyncHandler.util.js"
 
 
 const healthCheck = asyncHandler(async (req,res) =>{
-  return res.status(200).json(new ApiResponse(200,"OK","Health Check PASSED"))
+  return res.status(200).send(new ApiResponse(200,"OK","Health Check PASSED"))
 })
 
 export {healthCheck}
